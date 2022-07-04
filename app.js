0d4f0b351afc30adb40a067c6a82f79b8f851cab
Clam119 <Christopher.lam119@gmail.com>
2022-07-04 16:18:35 +0100

    Created separate public folder for imgs/css & created the front page design for the JSON generator with Bootstrap for quick & easy functionaltiy

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname + '/index.html')); 
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+    
+})
