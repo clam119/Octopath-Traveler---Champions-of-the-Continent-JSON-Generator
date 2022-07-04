bc4725431984d7c43f2c6483968264c45614e2ce
Clam119 <Christopher.lam119@gmail.com>
2022-07-04 20:36:08 +0100

    send public

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname + '/index.html')); 
     res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public');
 })
 
 app.listen(port, () => {
